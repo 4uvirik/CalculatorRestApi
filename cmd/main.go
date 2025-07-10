600d12c767001035d1ffbec6942c412d00436e22
4uvirik <chuvirik@yandex.ru>
2025-07-10 18:31:04 +0300

    fix(main): лог ошибки загрузки конфига с %v, запуск сервера с cfg

--- cmd/main.go
+++ cmd/main.go
@@ -11,12 +11,12 @@ func main() {
 
 	cfg, err := config.LoadConfig("config/config.json")
 	if err != nil {
-		log.Fatalf("config not loaded ")
+		log.Fatalf("failed to load config: %v", err)
 	}
 
-	fmt.Println("Порт:", cfg.Server)
+	fmt.Println("Порт:", cfg.Server.Port)
 	fmt.Println("Уровень логирования:", cfg.Logger.Level, "Формат лоигрвоания", cfg.Logger.Format)
 
-	server.RunEchoServer()
+	server.RunEchoServer(cfg)
 
 }
