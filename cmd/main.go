e37b088c82d43f65ea799358dd81d0b3c2e65a5d
4uvirik <chuvirik@yandex.ru>
2025-07-10 18:05:19 +0300

    feat: добавил конфиг для порта и логгера, загрузил в main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"CalculatorRestApi/config"
 	"CalculatorRestApi/internal/server"
+	"fmt"
+	"log"
 )
 
 func main() {
+
+	cfg, err := config.LoadConfig("config/config.json")
+	if err != nil {
+		log.Fatalf("config not loaded ")
+	}
+
+	fmt.Println("Порт:", cfg.Server)
+	fmt.Println("Уровень логирования:", cfg.Logger.Level, "Формат лоигрвоания", cfg.Logger.Format)
 
 	server.RunEchoServer()
 
--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Port string `json:"port"`
+	}
+	Logger struct {
+		Level  string `json:"level"`
+		Format string `json:"format"`
+	}
+}
+
+func LoadConfig(path string) (*Config, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	var cfg Config
+	if err := decoder.Decode(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
