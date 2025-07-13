523103eafd59be1d37f2d7411ecbd480fed85bb6
4uvirik <chuvirik@yandex.ru>
2025-07-13 11:03:20 +0300

    style: переписал функцию идиоматичнее

--- internal/calc/calc.go
+++ internal/calc/calc.go
@@ -1,8 +1,7 @@
 package calc
 
 // Подсчет суммы чисел в слайсе
-func SumNumbers(number []int) int {
-	sum := 0
+func SumNumbers(number []int) (sum int) {
 	for _, n := range number {
 		sum += n
 	}
