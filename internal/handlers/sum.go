701a647bd8b52326ec1f2a34b974e5fa1a4af564
4uvirik <chuvirik@yandex.ru>
2025-07-14 15:11:08 +0300

    refactor: поместил хэндлер суммы в отдельный файл

---
+++ internal/handlers/sum.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"CalculatorRestApi/internal/calc"
+	"CalculatorRestApi/internal/models"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func SumHandler(c echo.Context, logger *logrus.Logger, store *models.SafeStore) error {
+	var req models.SumRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, models.ErrorResponse{
+			Code:    400,
+			Message: "Ошибка запроса",
+		})
+	}
+
+	if len(req.Numbers) == 0 {
+		return c.JSON(http.StatusBadRequest, models.ErrorResponse{
+			Code:    400,
+			Message: "Нет полученных данных",
+		})
+	}
+
+	// Пробую использовать новый логер
+	logger.Infof("Получены числа: %v", req.Numbers)
+
+	result := calc.SumNumbers(req.Numbers)
+
+	// Генерация ключа на основе чисел
+	key := fmt.Sprint(req.Numbers)
+
+	// Сохранение в памяти ключ: значение
+	store.Save(key, result)
+
+	return c.JSON(http.StatusOK, models.SumResponse{Result: result})
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,51 +2,18 @@ package server
 
 import (
 	"CalculatorRestApi/config"
-	"CalculatorRestApi/internal/calc"
+	"CalculatorRestApi/internal/handlers"
 	"CalculatorRestApi/internal/models"
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
 func RunEchoServer(cfg *config.Config, logger *logrus.Logger, store *models.SafeStore) {
 	e := echo.New()
 
 	e.POST("/calculate/sum", func(c echo.Context) error {
-		return sumHandler(c, logger, store)
+		return handlers.SumHandler(c, logger, store)
 	})
 
 	e.Logger.Fatal(e.Start(cfg.Server.Port))
-}
-
-func sumHandler(c echo.Context, logger *logrus.Logger, store *models.SafeStore) error {
-	var req models.SumRequest
-
-	if err := c.Bind(&req); err != nil {
-		return c.JSON(http.StatusBadRequest, models.ErrorResponse{
-			Code:    400,
-			Message: "Ошибка запроса",
-		})
-	}
-
-	if len(req.Numbers) == 0 {
-		return c.JSON(http.StatusBadRequest, models.ErrorResponse{
-			Code:    400,
-			Message: "Нет полученных данных",
-		})
-	}
-
-	// Пробую использовать новый логер
-	logger.Infof("Получены числа: %v", req.Numbers)
-
-	result := calc.SumNumbers(req.Numbers)
-
-	// Генерация ключа на основе чисел
-	key := fmt.Sprint(req.Numbers)
-
-	// Сохранение в памяти ключ: значение
-	store.Save(key, result)
-
-	return c.JSON(http.StatusOK, models.SumResponse{Result: result})
 }
