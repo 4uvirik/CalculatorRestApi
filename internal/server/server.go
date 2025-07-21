cdb52c22709ca6b5cd9d6ea015d5cebc17fb139b
4uvirik <chuvirik@yandex.ru>
2025-07-21 15:23:11 +0300

    feat: Добавил swagger (swaggo)

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,10 +6,13 @@ import (
 	"CalculatorRestApi/internal/models"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 func RunEchoServer(cfg *config.Config, logger *logrus.Logger, store *models.SafeStore) {
 	e := echo.New()
+
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.POST("/calculate/sum", func(c echo.Context) error {
 		return handlers.SumHandler(c, logger, store)
