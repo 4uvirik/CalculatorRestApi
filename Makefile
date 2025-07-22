f04e41b0525a96adc5676121a93ed8b721a7dacd
4uvirik <chuvirik@yandex.ru>
2025-07-22 20:13:33 +0300

    fix test: исправил ошибки в тестах + добавил тесты в makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 APP_NAME := Calculator with REST API
 ENTRYPOINT := cmd/main.go
 
-.PHONY: docs
+.PHONY: docs tests cover
 
 docs:
 	swag init -g $(ENTRYPOINT)
+
+tests:
+	go test ./... -coverprofile=covarage.out
+
+cover:
+	go tool cover -html=covarage.out -o coverage.html
+	firefox coverage.html
--- internal/calc/multiply_test.go
+++ internal/calc/multiply_test.go
@@ -9,21 +9,22 @@ func TestMultiplyNumbers(t *testing.T) {
 		input    []int
 		expected int
 	}{
-		{"Пустой слайс", nil, 0},
+		{"Пустой слайс", nil, 1},
 		{"Нулевое значение", []int{0}, 0},
 		{"положительные числа", []int{2, 3, 5}, 30},
 		{"отрицательные числа", []int{-2, -3, -5}, -30},
-		{"разные числа", []int{-2, 0, 5}, -10},
+		{"разные числа", []int{-2, 1, 5}, -10},
+		{"умножение на 0", []int{-2, 0, 5}, 0},
 		{"одно число", []int{351}, 351},
-		{"большой слайс", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, 120},
-		{"большие числа", []int{111111, 555555, 999999}, 1666665},
+		{"большой слайс", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 479001600},
+		{"большие числа", []int{111111, 555555, 999999}, 61728209876728395},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := SumNumbers(tt.input)
+			actual := MultiplyNumbers(tt.input)
 			if actual != tt.expected {
-				t.Errorf("SumNumbers %v = %d ; want %d", tt.input, actual, tt.expected)
+				t.Errorf("MultiplyNumbers %v = %d ; want %d", tt.input, actual, tt.expected)
 			}
 		})
 	}
